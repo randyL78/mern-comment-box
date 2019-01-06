6180ddc8f5da0b4c06fd27359ffeedf840ad935f
Randy's Mac <randydavidl78@gmail.com>
2019-01-05 22:09:46 -0500

    add model for db

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -1,1 +1,13 @@
-const mongoos = require("mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// create a new instance of mongoose schema. describes shape of db entries
+const CommentsSchema = new Schema({
+    author: String,
+    text: String
+  },
+  {
+    timestamps: true
+  });
+
+module.exports = mongoose.model("Comment", CommentsSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express =  require('express');
-const bodyParser =  require('body-parser');
 const logger =  require('morgan');
 const mongoose =  require('mongoose');
+
+const Comment = require('./models/comment');
 
 const app = express();
 const router = express.Router();
 
-const API_PORT = process.env.API_PORT || 3001;
+// set our port to either a predetermined port number if you have set it up, or 3001
+app.set('port', process.env.PORT || 3001);
 
 // db config
 mongoose.connect("mongodb://localhost:27017/comments", { useNewUrlParser: true });
@@ -16,17 +18,49 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
 db.once('open', () => { console.log("Successfully connected to DataBase") });
 
-// set our port to either a predetermined port number if you have set it up, or 3001
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(logger('dev'));
+app.use(express.json());
 
 // set the route path & initialize the API
 router.get('/', (req, res) => {
    res.json({ message: 'Hello World' });
 });
 
+router.get('/comments', (req, res) => {
+   Comment.find((err, comments) => {
+      if (err) 
+         return  res.json({success: false, error: err});
+      return  res.json({success: true, data: comments});
+   });
+});
+
+router.post('/comments', (req, res) => {
+   const {author, text} = req.body;
+   const comment = new Comment();
+   if(!author) {
+      return  res.json({
+         success: false,
+         error: 'You must provide an author'
+      });
+   }
+   if (!text) {
+      return  res.json({
+         success: false,
+         error: 'You must provide a comment'
+      });
+   }
+   comment.author = author;
+   comment.text = text;
+   comment.save(err => {
+      if (err)
+         return  res.json({success: false, error: err});
+      return  res.json({success: true});
+   });
+});
+
 // Use our router configuration when we call /api
 app.use('/api', router);
 
-app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
+// start listening on our port
+const server = app.listen(app.get('port'), () => {
+   console.log(`Express server is listening on port ${server.address().port}`);
+ });
